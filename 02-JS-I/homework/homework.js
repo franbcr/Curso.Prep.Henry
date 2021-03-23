adcbd23d80d85f1b9c6f4415dd95afee0765d010
franbcr <fcbonanno@gmail.com>
2021-03-23 19:54:11 -0300

    02-JS-I/homework/homework.js - DONE

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hello World";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 12;
+const nuevoNum = 10;
 
 // Crea una variable booleana:
 const nuevoBool = true;
